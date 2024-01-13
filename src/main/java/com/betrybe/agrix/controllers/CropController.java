02ee14a4944514cca3e9f579b6e5edcafb39c858
Matheus Diógenes <mathdiogenes@gmail.com>
2024-01-12 21:19:44 -0300

    feat: R4 a R6 - Authorizations.

--- src/main/java/com/betrybe/agrix/controllers/CropController.java
+++ src/main/java/com/betrybe/agrix/controllers/CropController.java
@@ -10,6 +10,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,6 +39,7 @@ public class CropController {
    * @return the all crops
    */
   @GetMapping
+  @Secured({"ROLE_MANAGER", "ROLE_ADMIN"})
   public List<CropDto> getAllCrops() {
     List<Crop> cropList = this.cropService.getAllCrops();
     return cropList.stream()
--- src/main/java/com/betrybe/agrix/controllers/FarmController.java
+++ src/main/java/com/betrybe/agrix/controllers/FarmController.java
@@ -11,7 +11,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -61,7 +61,7 @@ public class FarmController {
    * @return the all farms
    */
   @GetMapping
-  @PreAuthorize("hasAnyAuthority('USER', 'MANAGER', 'ADMIN')")
+  @Secured({"ROLE_USER", "ROLE_MANAGER", "ROLE_ADMIN"})
   public List<FarmDto> getAllFarms() {
     List<Farm> farmList = this.farmService.getAllFarms();
     return farmList.stream()
